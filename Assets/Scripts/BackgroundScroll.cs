b6f7a66d0565d2797dc126ac3279b1b41350002b
HmihoK <nnana90909@gmail.com>
2025-09-29 23:12:50 +0900

    Background Scroll add

--- Assets/Scripts/BackgroundScroll.cs
+++ Assets/Scripts/BackgroundScroll.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Backgroundscroll : MonoBehaviour
+public class BackgroundScroll : MonoBehaviour
 {
+    [Header("Settings")]
+    [Tooltip("How fast should the texture scroll?")]
+    public float scrollSpeed;
+
+    [Header("References")]
+    public MeshRenderer meshRenderer;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,6 @@ public class Backgroundscroll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        meshRenderer.material.mainTextureOffset += new Vector2(scrollSpeed * Time.deltaTime, 0);
     }
 }
